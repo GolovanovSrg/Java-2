b413e6ec51aa7d5d4109e79c4dc865d09e272016
Golovanovsrg <Sergey_XG@mail.ru>
2016-09-11 09:25:03 +0300

    refactoring

--- Lazy/src/main/java/ru/spbau/mit/LazyFactory.java
+++ Lazy/src/main/java/ru/spbau/mit/LazyFactory.java
@@ -22,16 +22,16 @@ public class LazyFactory {
     }
 
     private static class OneThreadLazy<T> implements Lazy<T> {
-        private final Supplier<T> sup;
+        private final Supplier<T> supplier;
         private SupplierResult<T> result = new SupplierResult<>();
 
-        OneThreadLazy(Supplier<T> sup) {
-            this.sup = sup;
+        OneThreadLazy(Supplier<T> supplier) {
+            this.supplier = supplier;
         }
 
         public T get() {
             if (!result.isDone) {
-                result.value = sup.get();
+                result.value = supplier.get();
                 result.isDone = true;
             }
 
@@ -40,18 +40,18 @@ public class LazyFactory {
     }
 
     private static class MultiThreadLazy<T> implements Lazy<T> {
-        private final Supplier<T> sup;
+        private final Supplier<T> supplier;
         private volatile SupplierResult<T> result = new SupplierResult<>();
 
-        MultiThreadLazy(Supplier<T> sup) {
-            this.sup = sup;
+        MultiThreadLazy(Supplier<T> supplier) {
+            this.supplier = supplier;
         }
 
         public T get() {
             if (!result.isDone) {
                 synchronized (MultiThreadLazy.class) {
                     if (!result.isDone) {
-                        result.value = sup.get();
+                        result.value = supplier.get();
                         result.isDone = true;
                     }
                 }
@@ -62,16 +62,16 @@ public class LazyFactory {
     }
 
     private static class LockFreeLazy<T> implements Lazy<T> {
-        private final Supplier<T> sup;
+        private final Supplier<T> supplier;
         private AtomicReference<SupplierResult<T>> result = new AtomicReference<>(null);
 
-        LockFreeLazy(Supplier<T> sup) {
-            this.sup = sup;
+        LockFreeLazy(Supplier<T> supplier) {
+            this.supplier = supplier;
         }
 
         public T get() {
             if (result.get() == null) {
-                SupplierResult<T> curResult = new SupplierResult<>(sup.get(), true);
+                SupplierResult<T> curResult = new SupplierResult<>(supplier.get(), true);
                 result.compareAndSet(null, curResult);
             }
 
@@ -79,16 +79,16 @@ public class LazyFactory {
         }
     }
 
-    public static <T> Lazy<T> createOneThreadLazy(Supplier<T> sup) {
-        return new OneThreadLazy<T>(sup);
+    public static <T> Lazy<T> createOneThreadLazy(Supplier<T> supplier) {
+        return new OneThreadLazy<T>(supplier);
     }
 
-    public static <T> Lazy<T> createMultiThreadLazy(Supplier<T> sup) {
-        return new MultiThreadLazy<T>(sup);
+    public static <T> Lazy<T> createMultiThreadLazy(Supplier<T> supplier) {
+        return new MultiThreadLazy<T>(supplier);
     }
 
-    public static <T> Lazy<T> createLockFreeLazy(Supplier<T> sup) {
-        return new LockFreeLazy<T>(sup);
+    public static <T> Lazy<T> createLockFreeLazy(Supplier<T> supplier) {
+        return new LockFreeLazy<T>(supplier);
     }
 
 }
--- Lazy/test/java/ru/spbau/mit/LazyFactoryTest.java
+++ Lazy/test/java/ru/spbau/mit/LazyFactoryTest.java
@@ -1,8 +1,14 @@
 package ru.spbau.mit;
 
+import ru.spbau.mit.Lazy;
+import ru.spbau.mit.LazyFactory;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -19,32 +25,37 @@ public class LazyFactoryTest {
         assertEquals(firstValue, lazyObj.get());
     }
 
-    private <T> void testMultiThreadObj(final Lazy<T> obj, final int nThreads) {
+    private <T> void testMultiThreadObj(final Lazy<T> obj, final int nThreads) throws InterruptedException {
         List<T> threadValues = Collections.synchronizedList(new ArrayList<>());
 
-        for (int i = 0; i < nThreads; i++) {
-            Thread currentThread = new Thread(() -> threadValues.add(obj.get()));
-            currentThread.start();
+        ExecutorService service = Executors.newFixedThreadPool(nThreads);
+
+        int nTasks = 1000;
+        for (int i = 0; i < nTasks; i++) {
+            service.submit(() -> threadValues.add(obj.get()));
         }
 
-        while (threadValues.size() != nThreads) {}
+        service.shutdown();
+        service.awaitTermination(5, TimeUnit.SECONDS);
 
-        for (T val : threadValues) {
-            assertEquals(val, threadValues.get(0));
-        }
+        long countGoodValues = threadValues.stream()
+                                           .filter((val) -> val.equals(threadValues.get(0)))
+                                           .count();
+
+        assertEquals(countGoodValues, nTasks);
     }
 
     @org.junit.Test
     public void createMultiThreadLazy() throws Exception {
         final Lazy<Double> lazyObj = LazyFactory.createMultiThreadLazy(() -> Math.random());
-        final int nThreads = 1000;
+        final int nThreads = 3;
         testMultiThreadObj(lazyObj, nThreads);
     }
 
     @org.junit.Test
     public void createLockFreeLazy() throws Exception {
         final Lazy<Double> lazyObj = LazyFactory.createLockFreeLazy(() -> Math.random());
-        final int nThreads = 1000;
+        final int nThreads = 3;
         testMultiThreadObj(lazyObj, nThreads);
     }
 
